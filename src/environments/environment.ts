ef60d901fbc8c92654003c4ecaa1e78d0057c4c6
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-22 20:41:41 +0100

    Add redme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: false,
-    apiKey: "6337e215faac57eb1d61a1f6a37e200d",
-    units: "metric",
-    lang: "en",
-    baseUrlOpenWeather: 'https://api.openweathermap.org/data/2.5',
-  }; 
+  production: false,
+  apiKey: '',
+  units: 'metric',
+  lang: 'en',
+  baseUrlOpenWeather: 'https://api.openweathermap.org/data/2.5',
+};
