a54f720a8baf157734407b1d22ae118fa2fa0aca
Akim Benchiha <akim.benchiha@gmail.com>
2022-01-22 02:43:35 +0100

    Add env script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,1 @@
-export const environment = {
-  production: true,
-  apiKey: '6337e215faac57eb1d61a1f6a37e200d',
-  units: 'metric',
-  lang: 'en',
-  baseUrlOpenWeather: 'https://api.openweathermap.org/data/2.5/weather',
-};
+export const environment = {};
